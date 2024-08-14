05f13de0275eabfc456853efeb01b82d53a07caa
purushottamgutthe <purushottamgutthe6767@gmail.com>
2024-08-14 10:15:10 +0530

    Adding another case program

---
+++ caseProg2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter any number: " choice
+case $choice in
+	1) echo "valid choice";;
+	3) echo "valid choice";;
+	7) echo "valid choice";;
+	10)echo "valid choice";;
+	*) echo "No valid choice";;
+esac
